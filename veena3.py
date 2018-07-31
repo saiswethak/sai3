19db2d7b928fda0ae17444b1ff92225ea6bdb7e6
saiswethak <41769608+saiswethak@users.noreply.github.com>
2018-07-31 15:11:47 +0530

    Update veena3.py

--- veena3.py
+++ veena3.py
@@ -1,6 +1,6 @@
 val=input()
-if val>='a' and val<='z':
-    if val=='a' or val=='e' or val=='i' or val=='o' or val=='u':
+if val>='a' and val<='z' or val>='A' and val>='Z':
+    if val=='a' or val=='e' or val=='i' or val=='o' or val=='u' or val=='A' or val=='E' or val=='I' or val=='O' or val=='U':
         print("vowel")
     else:
         print ("consonant")
